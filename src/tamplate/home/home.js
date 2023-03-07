02376020d150e3a78138e7303ad932fda2f89518
CarolinaFuenzalida <carolinapazfuenzalidapoblete@gmail.com>
2023-03-07 12:36:55 -0300

    consumo api

--- src/tamplate/home/home.js
+++ src/tamplate/home/home.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-    return (
-        <div>
-            <header className="App-header">
-                <p>Home</p>
 
-            </header></div>
-    )
+
+const [result, setResult] = useState([]);
+
+
+useEffect(() => {
+  fetch('https://dummyjson.com/products')
+    .then((response) => {
+      return response.json()
+    })
+    .then((result) => {
+      setResult(result.products)
+    })
+}, [])
+
+console.log(result)
+
+
+  return (
+    <div className='productsDiv'>
+      {result.map((art, index) => {
+            return (
+              <div className='product'
+              key={index}>
+                    <img src={art.images[0]}/>
+                    <p >{art.title}</p>
+                    <p >{art.description}</p>
+                    <p >${art.price}</p>
+              </div>    
+            );
+          })}
+    </div>
+  )
 }
+
+
