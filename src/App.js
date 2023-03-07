cc9a3815df8e03547fab90ddfb16a9e43f8729c9
Web Connection Dayi <mariannybatista2003@gmail.com>
2023-03-07 13:59:09 -0300

    eliminando archivos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Login from './tamplate/login/login';
+
 import Navbar from './tamplate/navbar';
-import Logo from './componentes/logo';
+
 import Home from './tamplate/home/home';
-import Carrito from './tamplate/carrito.js/carrito';
+
 
 function App() {
   return (
